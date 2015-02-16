a6712b2bfd5357a18b08d31395c615df8e8a1dda
Victor B. <Ortherion@users.noreply.github.com>
2015-02-16 09:58:08 -0600

    Create vowelcount.java
    temporary

---
+++ vowelcount.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.math.*;
+
+class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int newCase = sc.nextInt();
+		char[] vowels = new char[]{'a', 'e', 'i', 'o', 'u', 'y'};
+		String input = sc.nextLine();
+
+		System.out.println(input);
+	}
+}	
