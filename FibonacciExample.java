81b8fd982a12265737bc4b5fa06806735f2ea57d
Victor B. <Ortherion@users.noreply.github.com>
2015-02-20 08:21:06 -0600

    Sample
    This is just a sample, not a solution to the codeabbey problem. Using the recursive function would create an absurdly long compile time. This is good only for the first few values of the Fibonacci sequence.

---
+++ FibonacciExample.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Fibo
+{
+	public static int fib(int n)
+	{
+		if (n < 2)
+		{
+			return n;
+		}
+		else
+		{
+			return fib(n-1) + fib(n-2);
+		}
+	}
+		
+	public static void main(String[] args)
+	{
+		System.out.println(fib(5));
+	}
+		
+}
